ed63428260a0775dbf7f657d436151b65973f989
Sir3nka <Harpunssserpss1>
2020-06-18 14:04:29 +0200

    Added unit tests veryfing pattern matching of hash

--- src/test/java/model/BlockTest.java
+++ src/test/java/model/BlockTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Stack;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class BlockTest {
@@ -18,7 +19,7 @@ public class BlockTest {
     @Test
     public void createBlocks() {
         Stack<Block> blockStack = new Stack<>();
-        Block.setLeadingZeros(6);
+        Block.setLeadingZeros(1);
         Block firstBlock = blockFabric.createFirstBlock();
         blockStack.push(firstBlock);
 
@@ -28,5 +29,37 @@ public class BlockTest {
                 });
 
         blockStack.forEach(System.out::println);
+    }
+
+    @Test
+    public void shouldContainLeadingZeros() {
+        Block.setLeadingZeros(1);
+        Block firstBlock = blockFabric.createFirstBlock();
+
+        Block.setLeadingZeros(1);
+        Pattern pattern = Pattern.compile("^0");
+        assert(pattern.matcher(firstBlock.getCurrentBlock()).find());
+
+        Block.setLeadingZeros(2);
+        firstBlock = blockFabric.createFirstBlock();
+        pattern = Pattern.compile("^00");
+        assert(pattern.matcher(firstBlock.getCurrentBlock()).find());
+
+        pattern = Pattern.compile("^000");
+        Block.setLeadingZeros(3);
+        firstBlock = blockFabric.createFirstBlock();
+        assert(pattern.matcher(firstBlock.getCurrentBlock()).find());
+    }
+
+    @Test
+    public void verifyStringRepresentation() {
+        Block.setLeadingZeros(1);
+        Block firstBlock = blockFabric.createFirstBlock();
+
+        assert(firstBlock.getPreviousBlock().equals("0"));
+        Pattern pattern = Pattern.compile("^0");
+        assert(pattern.matcher(firstBlock.getCurrentBlock()).find());
+
+        System.out.println(firstBlock.toString());
     }
 }
